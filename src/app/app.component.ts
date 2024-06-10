0981796085f0e6e10eafd7a5a5df82d6244f4bd3
KlaudiaLapisz <lapisz.klaudia@gmail.com>
2024-06-10 09:17:29 +0200

    Added new project

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { Customer } from '../model';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  customer: Customer = {
+    name: 'Klaudia',
+    age: 29,
+    isActive: true,
+  };
+}
